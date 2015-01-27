1166a7761f3f38353a2dbc79e54fa7eb2ca8646c
Thomas Jeatt <thomas@jea.tt>
2015-01-27 09:51:16 +0000

    Added uglify package

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,33 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
-var sass        = require('gulp-sass');
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify');
 
 gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "./"
-        }
-    });
+  browserSync({
+    server: {
+      baseDir: "./"
+    }
+  });
 });
 
 gulp.task('sass', function () {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/assets/css'))
-        .pipe(reload({stream:true}));
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(reload({stream:true}));
 });
 
 gulp.task('js', function() {
-	/// JS task
+  gulp.src('src/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/js'))
 });
 
 // Default task to be run with `gulp`
-gulp.task('watch', ['sass', 'browser-sync'], function () {
-    gulp.watch("src/scss/*.scss", ['sass']);
+gulp.task('watch', ['sass', 'js', 'browser-sync'], function () {
+  gulp.watch("src/scss/*.scss", ['sass']);
 });
 
 gulp.task('build', function() {
